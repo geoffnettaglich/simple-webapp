337ac337481b2e264cc67033534f78fe3c1d11f9
saem <saem@thinky.(none)>
2010-10-01 05:35:07 -0700

    switched to jetty.\nupdated the readme to markdown and put in some useful info, check it\nnoted the left over work to be figured out

--- src/main/java/com/geoffwebb/demo/controller/TopicController.java
+++ src/main/java/com/geoffwebb/demo/controller/TopicController.java
@@ -1,14 +1,16 @@
 package com.geoffwebb.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping(value="/")
 public class TopicController
 {
-    
+    @RequestMapping(method=RequestMethod.GET)
     public String show() 
     {
-        return "hello";
+        return "index";
     }
-
 }
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello this is a boring landing page</h2>
+<p>Check out the <a href="topic.htm">topics</a>.</p>
+</body>
+</html>
